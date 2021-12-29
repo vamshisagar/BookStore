0d15c7a662bdae8182c134b1f040a3886dd93bbc
Vamshi Sagar <vamshimolla@gmail.com>
2021-12-29 09:57:00 +0530

    AddedDbConnection

---
+++ BookStore/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+using BookStore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> categories { get; set; }
+        
+        
+    }
+}
